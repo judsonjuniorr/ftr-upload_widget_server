738672ab9c94c33208a31fdf9db1e8938f9e4ac7
Judson Cairo <judsonjuniorr@gmail.com>
2025-01-15 20:15:28 -0300

    feat: add file upload route with Swagger
    - Implement a route to handle file uploads.
    - Configure Swagger to support multipart/form-data for uploads.

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,16 +1,16 @@
 import { env } from '@/env'
+import { uploadImageRoute } from '@/infra/http/routes/upload-image'
+import { transformSwaggerSchema } from '@/infra/http/transform-swagger-schema'
 import { fastifyCors } from '@fastify/cors'
-import fastifyMultipart from '@fastify/multipart'
-import fastifySwagger from '@fastify/swagger'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
   hasZodFastifySchemaValidationErrors,
-  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
-import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
@@ -37,12 +37,14 @@ server.register(fastifySwagger, {
       version: '1.0.0',
     },
   },
-  transform: jsonSchemaTransform,
+  transform: transformSwaggerSchema,
 })
-server.register(fastifySwaggerUi)
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 server.register(uploadImageRoute)
 
 server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP Server running!')
+  console.log(`HTTP Server running on ${env.PORT}!`)
 })
