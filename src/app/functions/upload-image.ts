53bedfee2c428411c955b6ccb51877559f3753aa
Judson Cairo <judsonjuniorr@gmail.com>
2025-01-15 20:44:11 -0300

    feat: implement Either-based error handling
    - Introduced an Either class for error and success separation.
    - Eliminated excessive try-catch usage in favor of clearer error flow control.
    - Provided an example implementation demonstrating function result handling.
    - Improved error identification and code maintainability.

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -1,7 +1,10 @@
 import { Readable } from 'node:stream'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { makeLeft, makeRight } from '@/shared/either'
+import type { Either } from '@/shared/either'
 import { z } from 'zod'
+import { InvalidFileFormat } from './errors/invalid-file-format'
 
 const uploadImageInput = z.object({
   fileName: z.string(),
@@ -13,12 +16,14 @@ type UploadImageInput = z.input<typeof uploadImageInput>
 
 const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
 
-export async function uploadImage(input: UploadImageInput) {
+export async function uploadImage(
+  input: UploadImageInput
+): Promise<Either<InvalidFileFormat, { url: string }>> {
   const { constentStream, contentType, fileName } =
     uploadImageInput.parse(input)
 
   if (!allowedMimeTypes.includes(contentType)) {
-    throw new Error('Invalid file format.')
+    return makeLeft(new InvalidFileFormat())
   }
 
   // TODO: Carregar imagem p/ o Cloudflare R2
@@ -26,5 +31,9 @@ export async function uploadImage(input: UploadImageInput) {
     name: fileName,
     remoteKey: fileName,
     remoteUrl: fileName,
+  })
+
+  return makeRight({
+    url: '',
   })
 }
