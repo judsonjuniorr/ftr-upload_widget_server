670dffdad3a50896bd36d1c4a7952a992d20786c
Judson Cairo <judsonjuniorr@gmail.com>
2025-01-15 16:43:57 -0300

    feat: setup Fastify with Zod
    - Configured Fastify to integrate with Zod for validation and serialization
    - Added data validation for input and serialization for output
    - Implemented a global error handler for expected and unexpected errors
    - Structured routes with integrated Zod-based data typing
    - Emphasized automated testing integration

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,31 @@
 import { env } from '@/env'
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './routes/upload-image'
 
 const server = fastify()
 
+server.setSerializerCompiler(serializerCompiler)
+server.setValidatorCompiler(validatorCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.validation })
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(uploadImageRoute)
 
 server.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server running!')
