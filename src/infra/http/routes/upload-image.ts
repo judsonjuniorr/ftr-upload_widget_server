16693eda340d2076e5307959ee0e2250c06eb7dc
Judson Cairo <judsonjuniorr@gmail.com>
2025-01-15 21:46:05 -0300

    feat: handle large image upload limits
    - Implement handling for image uploads that exceed size limits.
    - Identify and return a specific error when the file size limit is exceeded.

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -33,6 +33,10 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         contentStream: uploadedFile.file,
       })
 
+      if (uploadedFile.file.truncated) {
+        return reply.status(400).send({ message: 'File is too large.' })
+      }
+
       if (isRight(result)) {
         const { url } = unwrapEither(result)
 
